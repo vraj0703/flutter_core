28d9498f88fae0a437ed3af622a2b9f59f058f9a
vraj0703 <vraj0703@gmail.com>
2025-09-08 01:47:42 +0530

    improve the classes

--- lib/base/pair.dart
+++ lib/base/pair.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/material.dart';
+
+@immutable
 class Pair<T, R> {
-  Pair(this.left, this.right);
+  const Pair(this.left, this.right);
 
   final T left;
 
   final R right;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true; // Optimization: same instance
+    return other is Pair<T, R> && // Check type and generic types
+        other.left == left &&
+        other.right == right;
+  }
+
+  @override
+  int get hashCode => left.hashCode ^ right.hashCode;
 
   @override
   String toString() => 'Pair[$left, $right]';
